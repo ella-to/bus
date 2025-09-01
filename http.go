b7348a782cf7c8ed5a8ab6053a67feba3154ad6a
Ali Najafizadeh <a.najafizadeh@gmail.com>
2025-09-01 09:04:03 -0400

    Add more logs and clean up Ack and Get methods

--- http.go
+++ http.go
@@ -1,7 +1,6 @@
 package bus
 
 import (
-	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -36,7 +35,6 @@ type Handler struct {
 	eventsLog     *immuta.Storage
 	waitingAckMap map[string]chan struct{} // {consumerId-eventId} -> ack
 	runner        task.Runner
-	buffer        bytes.Buffer
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -121,13 +119,14 @@ func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 	defer pusher.Close()
 
 	var startPos int64
-	if start == StartOldest {
+	switch start {
+	case StartOldest:
 		startPos = 0
 		start = ""
-	} else if start == StartNewest {
+	case StartNewest:
 		startPos = -1
 		start = ""
-	} else {
+	default:
 		// I wrote it like this to indicate the reader that
 		// we will start from the beginning of events log and
 		// loop through all events until we find the event id associate
@@ -172,42 +171,56 @@ func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
 
 		key := getAckKey(id, event.Id)
 
+		var ch chan struct{}
+
+		if ack == AckManual {
+			ch = make(chan struct{})
+
+			setAckMapError := h.runner.Submit(ctx, func(ctx context.Context) error {
+				h.waitingAckMap[key] = ch
+				return nil
+			}).Await(ctx)
+			if setAckMapError != nil {
+				slog.Error("failed to set signal channel for acking message", "error", setAckMapError, "consumer_id", id, "event_id", event.Id)
+				pusher.Push(newSseError(setAckMapError))
+				continue
+			}
+		}
+
 	REDLIVERY:
+		// NOTE: we are creating a new SSE msg since msg has some
+		// io.Read and io.Write internal variables which are essential to
+		// io.Copy
 		msg, err := newSseEvent(&event)
 		if err != nil {
+			slog.Warn("failed to create a sse message from event", "error", err, "event_id", event.Id, "consumer_id", id)
 			pusher.Push(newSseError(err))
 			continue
 		}
 
 		err = pusher.Push(msg)
 		if err != nil {
+			slog.Warn("failed to push sse message to consumer", "error", err, "event_id", event.Id, "consumer_id", id)
+			h.runner.Submit(ctx, func(ctx context.Context) error {
+				delete(h.waitingAckMap, key)
+				return nil
+			})
 			return
 		}
 
 		if ack == AckManual {
-			ch := make(chan struct{})
-
-			err := h.runner.Submit(ctx, func(ctx context.Context) error {
-				h.waitingAckMap[key] = ch
-				return nil
-			}).Await(ctx)
-			if err != nil {
-				pusher.Push(newSseError(err))
-				continue
-			}
-
 			select {
 			case <-ctx.Done():
-				return
-			case <-ch:
-				// ack received
-				slog.Info("received acked", "consumer_id", id, "event_id", event.Id)
 				h.runner.Submit(ctx, func(ctx context.Context) error {
 					delete(h.waitingAckMap, key)
 					return nil
-				}).Await(ctx)
+				})
+				return
+			case <-ch:
+				// ack received, the ch singal will be deleted by Ack function
+				slog.Debug("received acked", "consumer_id", id, "event_id", event.Id)
 			case <-time.After(redelivery):
-				slog.Info("redelivery", "consumer_id", id, "event_id", event.Id)
+				slog.Warn("redelivery", "consumer_id", id, "event_id", event.Id, "subject", event.Subject, "trace_id", event.TraceId)
 				h.runner.Submit(ctx, func(ctx context.Context) error {
 					delete(h.waitingAckMap, key)
 					return nil
@@ -252,6 +265,7 @@ func (h *Handler) Ack(w http.ResponseWriter, r *http.Request) {
 	err := h.runner.Submit(ctx, func(ctx context.Context) error {
 		ack, ok := h.waitingAckMap[key]
 		if !ok {
+			slog.Warn("failed to find ack channel key", "consumer_id", consumerId, "event_id", eventId)
 			return nil
 		}
 
